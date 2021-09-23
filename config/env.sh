48cdd5bb386d728d68d4d76b5bdef837a6d12e8c
Arbol <54153256+VictorAnizar@users.noreply.github.com>
2021-09-22 20:27:34 -0500

    Delete env.sh

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=4001
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://victor123:Hola1234!@cluster0.01uzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
